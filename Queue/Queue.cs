5775d83ed8278aaf48545d711caa53d5d8b9ed9b
Luizahf <luizahfreire@hotmail.com>
2019-07-24 13:29:38 -0300

    Queue structure and basic operations

---
+++ Queue/Queue.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue1
+{
+    class Queue
+    {
+        public class MyQueue<TNode>
+        {
+            private Node<TNode> Head;
+            private Node<TNode> Tail;
+
+            private class Node<TNode>
+            {
+                public TNode Data;
+                public Node<TNode> Next { get; set; }
+
+                public Node(TNode data)
+                {
+                    Data = data;
+                }
+
+                public override string ToString()
+                {
+                    return Data.ToString();
+                }
+            }
+
+            public void Push(TNode data)
+            {
+                Node<TNode> NewItem = new Node<TNode>(data);
+
+                if (Tail != null)
+                {
+                    Tail.next = NewItem;
+                }
+                Tail = NewItem;
+                if (Head == null)
+                {
+                    Head = NewItem;
+                }
+            }
+
+           
+            public void Pop(TNode data)
+            {
+                Head = Head.Next;
+                if (Head == null)
+                {
+                    Tail==null;
+                }
+                
+            }
+
+            public Boolean IsEmpty()
+            {
+                return Head == null;
+            }
+
+            public TNode Peek()
+            {
+                return Head.Data;
+            }
+        }
+    }
+}
