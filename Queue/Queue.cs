52fc34cabb9f5210d80a0fbd641c4e5ef72dbf57
Luizahf <luizahfreire@hotmail.com>
2019-07-26 12:10:21 -0300

    Fixed comments format to match C# documentation

--- Queue/Queue.cs
+++ Queue/Queue.cs
@@ -10,17 +10,30 @@ using System.Threading.Tasks;
  * 
  * Queue implementation through Nodes
  */
-
 namespace Queue1
 {
     class Queue
     {
+        /// <summary>
+        /// Class <c>MyQueue</c> sets the Queue data structure
+        /// <summary>
+        /// <param name="Front">
+        /// First element added to the Queue
+        /// </param>
+        /// <param name="Back">
+        /// Last element added to the Queue
+        /// </param>
         public class MyQueue<TNode>
         {
             private Node<TNode> Front;
             private Node<TNode> Back;
 
-            // Creates a Node with the data informed and sets the Next attr
+            /// <summary>
+            /// Class <c>Node</c> models the data informed in a node 
+            /// </summary>
+            /// <param name="Next"> 
+            /// Link to the next Node
+            /// </param>
             private class Node<TNode>
             {
                 public TNode Data;
@@ -37,23 +50,30 @@ namespace Queue1
                 }
             }
 
-            // Adds/Stores data as a Node in the back of the Queue
+            /// <summary>
+            /// Method <c>Enqueue</c> Adds/Stores data as a Node in the back of the Queue
+            /// </summary>
+            /// <param name="data">
+            /// Informed data that will be added to the Queue 
+            /// </param>
             public void Enqueue(TNode data)
             {
                 Node<TNode> NewItem = new Node<TNode>(data);
 
                 if (Back != null)
                 {
-                    Back.Next = NewItem;
+                    Back.next = NewItem;
                 }
-                Tail = NewItem;
+                Back = NewItem;
                 if (Front == null)
                 {
                     Front = NewItem;
                 }
             }
 
-            // Removes the first data Node of the Queue
+            /// <summary>
+            /// Method <c>Dequeue</c> Removes the first data Node of the Queue
+            /// </summary>
             public void Dequeue()
             {
                 Front = Front.Next;
@@ -64,13 +84,23 @@ namespace Queue1
 
             }
 
-            // Checks if the Queue is empty
+            /// <summary>
+            /// Method <c>IsEmpty</c> Checks if the Queue is empty
+            /// </summary>
+            /// <returns> 
+            /// Boolean value triggered when the Queue is null 
+            /// </returns>
             public Boolean IsEmpty()
             {
                 return Front == null;
             }
 
-            // Shows the element that's in the front of the Queue
+            /// <summary>
+            /// Method <c>getFront</c> Shows the element that's in the front of the Queue
+            /// </summary>
+            /// <returns> 
+            /// <c>Front</c> value: The first element int he Queue
+            /// </returns>
             public TNode getFront()
             {
                 return Front.Data;
