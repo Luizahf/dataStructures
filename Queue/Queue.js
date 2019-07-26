876e1cf6116ee6a44dc0d3e958138ac8120de66e
Luizahf <luizahfreire@hotmail.com>
2019-07-26 12:24:55 -0300

    Fixed comments to match JS documentation

--- Queue/Queue.js
+++ Queue/Queue.js
@@ -5,21 +5,35 @@
  * Queue implementation through Nodes
  */
 
-// Creates a Node with the data informed and sets the Next attr
+/**
+ * Creates a Node with the data informed and sets the Next attr
+ * @class
+ * @param  {generic} data Informed data to construct the Node
+ * @param  {next} null Link to the next node
+ */
 class Node {
   constructor(data) {
     (this.data = data), (this.next = null);
   }
 }
 
+/**
+ * Sets the front and back attrs for the queue
+ * @class
+ * @param  {Node} front First element added to the Queue
+ * @param  {Node} back Last element added to the Queue
+ */
 class Queue {
-  // Sets the front and back attrs for the queue
   constructor() {
     this.front = null;
     this.back = null;
   }
 
-  // Adds/Stores data as a Node in the back of the Queue
+  /**
+   * Adds/Stores data as a Node in the back of the Queue
+   * @class
+   * @param  {generic} data Data informed to create a node and be added to the Queue
+   */
   enqueue = data => {
     var newNode = new Node(data);
     if (this.back) {
@@ -31,19 +45,28 @@ class Queue {
     }
   };
 
-  // Removes the first data Node of the Queue
+  /**
+   * Removes the first data Node of the Queue
+   * @class
+   */
   dequeue = () => {
     if (this.front) {
       this.front = front.next;
     }
   };
 
-  // Checks if the Queue is empty
+  /**
+   * Checks if the Queue is empty
+   * @returns {boolean} Boolean value triggered when the Queue is null
+   */
   isEmpty = () => {
     return this.front == null;
   };
 
-  // Shows the element that's in the front of the Queue
+  /**
+   * Shows the element that's in the front of the Queue
+   * @returns {Node} The first element int he Queue
+   */
   getFront = () => {
     return this.front;
   };
